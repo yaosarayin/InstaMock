62de8e2d5437beec4f5830350bf6aa783ebaf4aa
Yao <yyin@college.harvard.edu>
2020-10-23 20:36:15 -0400

    Initial Commit

---
+++ InstaMock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaMock
+//
+//  Created by Yao Yin on 10/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
