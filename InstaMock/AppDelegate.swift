0296f95ea21fc3ab6c24972b05ff6d76c0d12693
Yao <yyin@college.harvard.edu>
2020-10-23 22:24:30 -0400

    InstaMock Part 1

--- InstaMock/AppDelegate.swift
+++ InstaMock/AppDelegate.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        // --- Copy this only
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "JlSsTLg0d3rHIotfetBBbE0PnRzLxRcCMwdHaBvg" // <- UPDATE
+                $0.clientKey = "FytubQ5YIQFyvpV2esXzDKsbtw3eO1p0T5mVEzcW" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
+        // --- end copy
+
+
         return true
     }
 
@@ -30,7 +43,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    
+    
 
 }
 
